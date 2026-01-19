c75f36f8622cead1da08efd215d6f4ab124b005b
GoshaN <eeewwweeerrr1@gmail.com>
2026-01-19 16:10:50 +0500

    Изменил настройки vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
+// import { defineConfig } from 'vite';
+// import { resolve } from 'path';
+
+// export default defineConfig({
+//   root: '.', // корень проекта
+//   build: {
+//     outDir: 'dist',     // папка для сборки
+//     emptyOutDir: true,  // очистка dist перед сборкой
+//     rollupOptions: {
+//       input: resolve(__dirname, 'index.html'), // точка входа — index.html
+//     },
+//   },
+//   server: {
+//     open: true,     // автоматически открывать страницу в браузере
+//     port: 3000,     // порт dev-сервера
+//   },
+//   base: './',
+// });
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  root: '.', // корень проекта
-  build: {
-    outDir: 'dist',     // папка для сборки
-    emptyOutDir: true,  // очистка dist перед сборкой
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'), // точка входа — index.html
-    },
-  },
   server: {
-    open: true,     // автоматически открывать страницу в браузере
-    port: 3000,     // порт dev-сервера
+    open: true,
   },
   base: './',
 });
